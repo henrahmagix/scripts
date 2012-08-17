aa1a4da4c76803f948b226ad71bfba9b643c743c
henry <henry@incuna.com>
2012-08-17 14:09:53 +0100

    Forgot to exit on bad usage, and added a todo.

--- change_all_desktops.sh
+++ change_all_desktops.sh
@@ -3,8 +3,11 @@
 # https://discussions.apple.com/message/18805586#18805586
 # https://applehelpwriter.wordpress.com/category/mountain-lion/
 
+# Exit if no image provided.
+# @TODO check for file extension and file existence?
 if [[ $# -lt 1 ]]; then
 	echo "No image path provided."
+	exit 1
 fi
 
 echo "Change all desktop backgrounds to $1"
