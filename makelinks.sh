0ccad5a5c7812942324c7bba5435e48ed27d61ee
henry <henry@incuna.com>
2012-08-18 11:08:21 +0100

    Forcing symlink creation to overwrite currents.

--- makelinks.sh
+++ makelinks.sh
@@ -2,12 +2,12 @@
 
 script="/usr/local/scripts"
 bin="/usr/local/bin"
-apps="~/Apps"
+apps="$HOME/Apps"
 
-ln -s $script/debian_rename $bin/rename
-ln -s $script/handbrake_batch.sh $bin/handbrake-batch
-ln -s $script/number_of_files.sh $bin/numfiles
-ln -s $script/change_all_desktops.sh $bin/chbg
-ln -s $apps/todo.sh $bin/
-ln -s $apps/HandBrakeCLI $bin/
-ln -s $apps/mvim $bin
+ln -f -s $script/debian_rename $bin/rename
+ln -f -s $script/handbrake_batch.sh $bin/handbrake-batch
+ln -f -s $script/number_of_files.sh $bin/numfiles
+ln -f -s $script/change_all_desktops.sh $bin/chbg
+ln -f -s $apps/todo.sh $bin/
+ln -f -s $apps/HandBrakeCLI $bin/
+ln -f -s $apps/mvim $bin
