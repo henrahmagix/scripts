9e8a866be17de41647c98656c183130630c106df
henry <henry@incuna.com>
2012-08-19 02:31:24 +0100

    Added "Is this file a symlink?" script, and updated install script to check for /usr/local/scripts already existing, and makelinks.sh to symlink is_symlink into bin without underscores, so issymlink.

--- makelinks.sh
+++ makelinks.sh
@@ -8,6 +8,7 @@ ln -s $script/debian_rename $bin/rename
 ln -s $script/handbrake_batch.sh $bin/handbrake-batch
 ln -s $script/number_of_files.sh $bin/numfiles
 ln -s $script/change_all_desktops.sh $bin/changebackground
+ln -s $script/is_symlink.sh $bin/issymlink
 ln -s $apps/todo.sh $bin/
 ln -s $apps/HandBrakeCLI $bin/
 ln -s $apps/mvim $bin/
