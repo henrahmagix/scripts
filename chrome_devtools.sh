06a3dbfd04da195b51fedf00948546f2210440d7
henry <henry@incuna.com>
2012-08-20 10:56:28 +0100

    Call to open Google Chrome Canary with a fresh profile, stored at ~/.chrome-devtools

---
+++ chrome_devtools.sh
@@ -0,0 +1,4 @@
+# Open development Chrome.
+/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary \
+--user-data-dir=$HOME/.chrome_devtools \
+--enable-experimental-extension-apis \
