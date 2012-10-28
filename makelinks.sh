6b1930b421e3e5750c97026270a633f33dac125d
henry <henry@incuna.com>
2012-10-28 12:13:30 +0000

    Tab separate makelinks script, and add change_dns.sh to it

--- makelinks.sh
+++ makelinks.sh
@@ -4,15 +4,16 @@ script="/usr/local/scripts"
 bin="/usr/local/bin"
 apps="$HOME/Apps"
 
-ln -sv $script/debian_rename $bin/rename
-ln -sv $script/handbrake_batch.sh $bin/handbrake-batch
-ln -sv $script/number_of_files.sh $bin/numfiles
-ln -sv $script/change_all_desktops.sh $bin/changebackground
-ln -sv $script/is_symlink.sh $bin/issymlink
-ln -sv $script/git_single_branch.sh $bin/git_single_branch
-ln -sv $script/git_delete_branches.sh $bin/git_delete_branches
-ln -sv $script/ghi $bin/ghi
-ln -sv $script/confirm.sh $bin/confirm.sh
-ln -sv $apps/todo.sh $bin/
-ln -sv $apps/HandBrakeCLI $bin/
-ln -sv $apps/mvim $bin/
+ln -sv $script/debian_rename		$bin/rename
+ln -sv $script/handbrake_batch.sh	$bin/handbrake-batch
+ln -sv $script/number_of_files.sh	$bin/numfiles
+ln -sv $script/change_all_desktops.sh	$bin/changebackground
+ln -sv $script/change_dns.sh		$bin/changedns
+ln -sv $script/is_symlink.sh		$bin/issymlink
+ln -sv $script/git_single_branch.sh	$bin/git_single_branch
+ln -sv $script/git_delete_branches.sh	$bin/git_delete_branches
+ln -sv $script/ghi			$bin/
+ln -sv $script/confirm.sh		$bin/
+ln -sv $apps/todo.sh			$bin/
+ln -sv $apps/HandBrakeCLI		$bin/
+ln -sv $apps/mvim			$bin/
