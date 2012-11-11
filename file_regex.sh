a0b11969c3eda94663fc2382188f6334e068f7bf
henry <henry@incuna.com>
2012-11-11 15:36:58 +0000

    Add a test/dryrun option by not passing -i to perl -p

--- file_regex.sh
+++ file_regex.sh
@@ -1,1 +1,6 @@
-ack -H --nocolor "$1" | cut -d":" -f1 | xargs perl -pi -e "s,$1,$2,"
+INPLACE="-i"
+if [[ $1 =~ ^-t$ ]] || [[ $1 =~ ^-test$ ]]; then
+    shift
+    INPLACE=""
+fi
+ack -H --nocolor "$1" | cut -d":" -f1 | xargs perl -p `echo $INPLACE` -e "s,$1,$2,"
