30cf381d667cb3b5b106af8ea414013b0c85a432
henry <henry@incuna.com>
2012-11-18 13:59:46 +0000

    Ensure whitespace in files is not expanded when passed to xargs

--- file_regex.sh
+++ file_regex.sh
@@ -1,6 +1,6 @@
 INPLACE="-i"
-if [[ $1 =~ ^-t$ ]] || [[ $1 =~ ^-test$ ]]; then
+if [[ $1 =~ ^-t$ ]] || [[ $1 =~ ^--test$ ]]; then
     shift
     INPLACE=""
 fi
-ack -H --nocolor "$1" | cut -d":" -f1 | xargs perl -p `echo $INPLACE` -e "s,$1,$2,"
+ack -H --nocolor "$1" | cut -d":" -f1 | xargs -I file perl -p `echo $INPLACE` -e "s,$1,$2," "file"
