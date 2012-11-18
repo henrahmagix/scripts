3474b74f72403a66dd8a5a3c2ef2149a9fb89409
henry <henry@incuna.com>
2012-11-18 14:36:08 +0000

    Add ack -a option and use getopts to get all options

--- file_regex.sh
+++ file_regex.sh
@@ -1,6 +1,15 @@
+ALL=""
 INPLACE="-i"
-if [[ $1 =~ ^-t$ ]] || [[ $1 =~ ^--test$ ]]; then
-    shift
-    INPLACE=""
-fi
-ack -H --nocolor "$1" | cut -d":" -f1 | xargs -I file perl -p `echo $INPLACE` -e "s,$1,$2," "file"
+opts="at-"
+while getopts $opts option; do
+    case $option in
+        a)
+            ALL="-a"
+            ;;
+        t)
+            INPLACE=""
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+ack -H --nocolor `echo $ALL` "$1" | cut -d":" -f1 | xargs -I file perl -p `echo $INPLACE` -e "s,$1,$2," "file"
