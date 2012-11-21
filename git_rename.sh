b7a27acc72cb654e3c85ea38dbb3e9e28d0d8e5e
henry <henry@incuna.com>
2012-11-21 16:25:20 +0000

    Remove confirm from git_rename.
    Git will be tracking the file changes anyway, so there's no need
    to confirm each change.

--- git_rename.sh
+++ git_rename.sh
@@ -1,10 +1,9 @@
 [ $# -lt 3 ] && echo "usage: $0 pattern replacestring files [...]" && exit 1
-source `which confirm.sh`
 pattern=$1
 shift
 replace=$1
 shift
 for x in $@
 do
-    confirm "rename ${x} --> ${x/$pattern/$replace} ?" && git mv "${x}" "${x/$pattern/$replace}"
+    git mv "${x}" "${x/$pattern/$replace}"
 done
