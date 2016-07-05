84a51459ed5436b89accce2a154b0721596b747d
Henry Blyth <blyth.henry@gmail.com>
2016-07-05 15:01:31 +0100

    Add diff-ignore-reorder git command

---
+++ git-diff-ignore-reorder.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+# from http://stackoverflow.com/a/21779343/3150057
+git diff | awk '{ seen[substr($0,2)]++; l[i++] = $0; } END { for (j = 0; j < i; ++j) if (seen[substr(l[j],2)] < 2) print l[j] }' | grep '^[-+]'
