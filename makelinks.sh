e8891573cf2323e96a6e3df5cc6a6717ea591136
Henry Blyth <blyth.henry@gmail.com>
2017-06-28 10:16:21 +0100

    Only link script if it exists

--- makelinks.sh
+++ makelinks.sh
@@ -4,20 +4,28 @@ script="/usr/local/scripts"
 bin="/usr/local/bin"
 apps="$HOME/Apps"
 
-ln -sv $script/change_all_desktops.sh       $bin/changebackground
-ln -sv $script/change_dns.sh                $bin/changedns
-ln -sv $script/compare_md5.sh               $bin/compare-md5
-ln -sv $script/confirm.sh                   $bin/
-ln -sv $script/file_regex.sh                $bin/file-regex
-ln -sv $script/git_single_branch.sh         $bin/git-single-branch
-ln -sv $script/git_delete_branches.sh       $bin/git-delete-branches
-ln -sv $script/git_rename.sh                $bin/git-rename
-ln -sv $script/git-diff-ignore-reorder.sh   $bin/git-diff-ignore-reorder
-ln -sv $script/handbrake_batch.sh           $bin/handbrake-batch
-ln -sv $script/is_symlink.sh                $bin/issymlink
-ln -sv $script/make_bootable.sh             $bin/make-bootable
-ln -sv $script/number_of_files.sh           $bin/numfiles
-ln -sv $script/debian_rename                $bin/rename
-ln -sv $apps/todo.sh                        $bin/
-ln -sv $apps/HandBrakeCLI                   $bin/
-ln -sv $apps/mvim                           $bin/
+link () {
+    if [ -f $1 ]; then
+        ln -sv $1 $2
+    else
+        echo "No script to link: $1"
+    fi
+}
+
+link $script/change_all_desktops.sh       $bin/changebackground
+link $script/change_dns.sh                $bin/changedns
+link $script/compare_md5.sh               $bin/compare-md5
+link $script/confirm.sh                   $bin/
+link $script/file_regex.sh                $bin/file-regex
+link $script/git_single_branch.sh         $bin/git-single-branch
+link $script/git_delete_branches.sh       $bin/git-delete-branches
+link $script/git_rename.sh                $bin/git-rename
+link $script/git-diff-ignore-reorder.sh   $bin/git-diff-ignore-reorder
+link $script/handbrake_batch.sh           $bin/handbrake-batch
+link $script/is_symlink.sh                $bin/issymlink
+link $script/make_bootable.sh             $bin/make-bootable
+link $script/number_of_files.sh           $bin/numfiles
+link $script/debian_rename                $bin/rename
+link $apps/todo.sh                        $bin/
+link $apps/HandBrakeCLI                   $bin/
+link $apps/mvim                           $bin/
