afb936bfdbc365504afe81f6d05d106b8bf35981
Henry Blyth <blyth.henry@gmail.com>
2018-03-16 13:59:59 +0000

    Fix newproject turning $HOME into a git repo
    No dir given means `cd` is run, which switches to `$HOME` booooooooooooo

--- newproject.sh
+++ newproject.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z $1 ]; then
+    echo "Folder name is required"
+    exit 1
+fi
+
 dir=$1
 title="$(echo $dir | sed -e 's/-/ /g' | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1')"
 
