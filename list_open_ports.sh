c0b203ab4c90fc4f61d5bc66848d8636f7dce1ab
Henry Blyth <blyth.henry@gmail.com>
2021-04-16 15:15:00 +0100

    Only list open ports for current user

--- list_open_ports.sh
+++ list_open_ports.sh
@@ -3,4 +3,4 @@ while IFS= read -r line; do
   echo "$line"
   pid="$(echo "$line" | tr -s ' ' | cut -d' ' -f2)"
   ps -p $pid | sed '1d'
-done <<< $(sudo lsof -PiTCP -sTCP:LISTEN | ag henryblyth)
+done <<< $(lsof -PiTCP -sTCP:LISTEN)
